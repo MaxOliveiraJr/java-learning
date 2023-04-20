893209de8009321fae5bc3766cd2a29edff3df2c
Max Oliveira <max-oliveira@outlook.com.br>
2023-04-20 15:15:16 -0300

    substituindo aluno no array

--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -298,7 +298,7 @@ public class PrimeiraClasseJava {
 		aluno5.setSerieMatriculado(serie);
 		aluno5.setNomeEscola(escola);*/
 		
-		for(int pos = 1;pos <= 4;pos++) {
+		for(int pos = 1;pos <= 1;pos++) {
 			String nomeDisciplina = JOptionPane.showInputDialog("Nome da Disciplica "+pos+" ?");
 			String notaDisciplina = JOptionPane.showInputDialog("Nota da Disciplica "+pos+" ?");
 			
@@ -380,6 +380,19 @@ public class PrimeiraClasseJava {
 		for (int pos =0;pos < alunos.size();pos++) {
 			
 			Aluno aluno = alunos.get(pos);
+			
+			if(aluno.getNome().equalsIgnoreCase("Max")) {
+				Aluno trocar = new Aluno();
+				trocar.setNome("Aluno trocado");
+				
+				Disciplina disciplina = new Disciplina();
+				disciplina.setDisciplina("Matematica");
+				disciplina.setNota(96);
+				trocar.getDisciplinas().add(disciplina)	;
+				
+				alunos.set(pos, trocar);
+				aluno = alunos.get(pos);
+			}
 			
 			System.out.println("Aluno = "+aluno.getNome());
 			System.out.println("Média = "+aluno.getMediaNota());
